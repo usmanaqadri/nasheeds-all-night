ff633cbdd1160fcfb0f9c9fbc77577f810e14c37
usmanaqadri <usmanaqadri@gmail.com>
2022-11-12 19:57:35 -0500

    jashne

--- src/NasheedBoard.js
+++ src/NasheedBoard.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React from "react";
 
-function NasheedBoard({nasheeds, onClick}) {
-	
-	const myNasheeds = [];
+function NasheedBoard({ nasheeds, onClick }) {
+  const myNasheeds = [];
 
-	nasheeds.forEach(element => myNasheeds.push(element.engTitle))
+  nasheeds.forEach((element) => {
+    const modifiedTitle =
+      element.engTitle.length < 15
+        ? element.engTitle
+        : element.engTitle.substring(0, 15);
+    myNasheeds.push(modifiedTitle);
+  });
 
-	const nasheedDivs = myNasheeds.map((nasheed, index)=> 
+  const nasheedDivs = myNasheeds.map((nasheed, index) => (
     <div className="nasheed-buttons" key={index}>
-      <button onClick={onClick(index)}  className="button-64"><span className="text">{nasheed}</span></button>
+      <button onClick={onClick(index)} className="button-64">
+        <span className="text">{nasheed}</span>
+      </button>
     </div>
-    );
+  ));
 
-	return (
-		<div>
-			<div className="nasheedBoard">{nasheedDivs}</div>
-		</div>
-	)
+  return (
+    <div>
+      <div className="nasheedBoard">{nasheedDivs}</div>
+    </div>
+  );
 }
 
-export default NasheedBoard
-
+export default NasheedBoard;
--- src/generateNasheed.js
+++ src/generateNasheed.js
@@ -1,155 +1,276 @@
-import badrArab from './nasheeds/salatul_badriyya_arb.txt'
-import badrEng from './nasheeds/salatul_badriyya_eng.txt'
-import badrRom from './nasheeds/salatul_badriyya_rom.txt'
-import talamaArab from './nasheeds/talama_arb.txt'
-import talamaEng from './nasheeds/talama_eng.txt'
-import talamaRom from './nasheeds/talama_rom.txt'
-import muhammadiyaArab from './nasheeds/muhammadiya_arb.txt'
-import muhammadiyaEng from './nasheeds/muhammadiya_eng.txt'
-import muhammadiyaRom from './nasheeds/muhammadiya_rom.txt'
-import burdahArab from './nasheeds/burdah_arb.txt'
-import burdahEng from './nasheeds/burdah_eng.txt'
-import burdahRom from './nasheeds/burdah_rom.txt'
-import ya_hadiyArab from './nasheeds/ya-hadiy_arb.txt'
-import ya_hadiyEng from './nasheeds/ya-hadiy_eng.txt'
-import ya_hadiyRom from './nasheeds/ya-hadiy_rom.txt'
-import ishrabArab from './nasheeds/ishrab_arb.txt'
-import ishrabEng from './nasheeds/ishrab_eng.txt'
-import ishrabRom from './nasheeds/ishrab_rom.txt'
-import ya_hananaArab from './nasheeds/ya-hanana_arb.txt'
-import ya_hananaEng from './nasheeds/ya-hanana_eng.txt'
-import ya_hananaRom from './nasheeds/ya-hanana_rom.txt'
-import talalArab from './nasheeds/talal_arb.txt'
-import talalEng from './nasheeds/talal_eng.txt'
-import talalRom from './nasheeds/talal_rom.txt'
-import hamdArab from './nasheeds/hamd_arb.txt'
-import hamdEng from './nasheeds/hamd_eng.txt'
-import hamdRom from './nasheeds/hamd_rom.txt'
-import lamyatiArab from './nasheeds/lamyati_arb.txt'
-import lamyatiEng from './nasheeds/lamyati_eng.txt'
-import lamyatiRom from './nasheeds/lamyati_rom.txt'
-import qadkafaniArab from './nasheeds/qadkafani_arb.txt'
-import qadkafaniEng from './nasheeds/qadkafani_eng.txt'
-import qadkafaniRom from './nasheeds/qadkafani_rom.txt'
-import yaalimanArab from './nasheeds/yaaliman_arb.txt'
-import yaalimanEng from './nasheeds/yaaliman_eng.txt'
-import yaalimanRom from './nasheeds/yaaliman_rom.txt'
-import salliyasalaamArab from './nasheeds/salliyasalaam_arb.txt'
-import salliyasalaamEng from './nasheeds/salliyasalaam_eng.txt'
-import salliyasalaamRom from './nasheeds/salliyasalaam_rom.txt'
-import araftuArab from './nasheeds/araftu_arb.txt'
-import araftuEng from './nasheeds/araftu_eng.txt'
-import araftuRom from './nasheeds/araftu_rom.txt'
-import salaamArab from './nasheeds/salaam_arb.txt'
-import salaamEng from './nasheeds/salaam_eng.txt'
-import salaamRom from './nasheeds/salaam_rom.txt'
-import assubhuArab from './nasheeds/assubhu_arb.txt'
-import assubhuEng from './nasheeds/assubhu_eng.txt'
-import assubhuRom from './nasheeds/assubhu_rom.txt'
-import bihayathbutArab from './nasheeds/bihayathbut_arb.txt'
-import bihayathbutEng from './nasheeds/bihayathbut_eng.txt'
-import bihayathbutRom from './nasheeds/bihayathbut_rom.txt'
-import madadArab from './nasheeds/madad_arb.txt'
-import madadEng from './nasheeds/madad_eng.txt'
-import madadRom from './nasheeds/madad_rom.txt'
-import yanabiArab from './nasheeds/yanabi_arb.txt'
-import yanabiEng from './nasheeds/yanabi_eng.txt'
-import yanabiRom from './nasheeds/yanabi_rom.txt'
-import yarabbiArab from './nasheeds/yarabbi_arb.txt'
-import yarabbiEng from './nasheeds/yarabbi_eng.txt'
-import yarabbiRom from './nasheeds/yarabbi_rom.txt'
-import faslonArab from './nasheeds/faslon_arb.txt'
-import faslonEng from './nasheeds/faslon_eng.txt'
-import faslonRom from './nasheeds/faslon_rom.txt'
-import iduArab from './nasheeds/idu_arb.txt'
-import iduEng from './nasheeds/idu_eng.txt'
-import iduRom from './nasheeds/idu_rom.txt'
-import haqiqatArab from './nasheeds/haqiqat_arb.txt'
-import haqiqatEng from './nasheeds/haqiqat_eng.txt'
-import haqiqatRom from './nasheeds/haqiqat_rom.txt'
-import ulfatArab from './nasheeds/ulfat_arb.txt'
-import ulfatEng from './nasheeds/ulfat_eng.txt'
-import ulfatRom from './nasheeds/ulfat_rom.txt'
-import zaminArab from './nasheeds/zamin_arb.txt'
-import zaminEng from './nasheeds/zamin_eng.txt'
-import zaminRom from './nasheeds/zamin_rom.txt'
-import saliqaArab from './nasheeds/saliqa_arb.txt'
-import saliqaEng from './nasheeds/saliqa_eng.txt'
-import saliqaRom from './nasheeds/saliqa_rom.txt'
-import huzurArab from './nasheeds/huzur_arb.txt'
-import huzurEng from './nasheeds/huzur_eng.txt'
-import huzurRom from './nasheeds/huzur_rom.txt'
-import paighamArab from './nasheeds/paigham_arb.txt'
-import paighamEng from './nasheeds/paigham_eng.txt'
-import paighamRom from './nasheeds/paigham_rom.txt'
-import yaNabiArab from './nasheeds/ya-nabi_arb.txt'
-import yaNabiEng from './nasheeds/ya-nabi_eng.txt'
-import yaNabiRom from './nasheeds/ya-nabi_rom.txt'
-import zaheArab from './nasheeds/zahe_arb.txt'
-import zaheEng from './nasheeds/zahe_eng.txt'
-import zaheRom from './nasheeds/zahe_rom.txt'
-import kaabeArab from './nasheeds/kaabe_arb.txt'
-import kaabeEng from './nasheeds/kaabe_eng.txt'
-import kaabeRom from './nasheeds/kaabe_rom.txt'
-import jabArab from './nasheeds/jab_arb.txt'
-import jabEng from './nasheeds/jab_eng.txt'
-import jabRom from './nasheeds/jab_rom.txt'
-import rokArab from './nasheeds/rok_arb.txt'
-import rokEng from './nasheeds/rok_eng.txt'
-import rokRom from './nasheeds/rok_rom.txt'
-import nakahinArab from './nasheeds/nakahin_arb.txt'
-import nakahinEng from './nasheeds/nakahin_eng.txt'
-import nakahinRom from './nasheeds/nakahin_rom.txt'
-import kawakibArab from './nasheeds/kawakib_arb.txt'
-import kawakibEng from './nasheeds/kawakib_eng.txt'
-import kawakibRom from './nasheeds/kawakib_rom.txt'
-import qamarunArab from './nasheeds/qamarun_arb.txt'
-import qamarunEng from './nasheeds/qamarun_eng.txt'
-import qamarunRom from './nasheeds/qamarun_rom.txt'
-import imamArab from './nasheeds/imam_arb.txt'
-import imamEng from './nasheeds/imam_eng.txt'
-import imamRom from './nasheeds/imam_rom.txt'
-import bashhirArab from './nasheeds/bashhir_arb.txt'
-import bashhirEng from './nasheeds/bashhir_eng.txt'
-import bashhirRom from './nasheeds/bashhir_rom.txt'
+import badrArab from "./nasheeds/salatul_badriyya_arb.txt";
+import badrEng from "./nasheeds/salatul_badriyya_eng.txt";
+import badrRom from "./nasheeds/salatul_badriyya_rom.txt";
+import talamaArab from "./nasheeds/talama_arb.txt";
+import talamaEng from "./nasheeds/talama_eng.txt";
+import talamaRom from "./nasheeds/talama_rom.txt";
+import muhammadiyaArab from "./nasheeds/muhammadiya_arb.txt";
+import muhammadiyaEng from "./nasheeds/muhammadiya_eng.txt";
+import muhammadiyaRom from "./nasheeds/muhammadiya_rom.txt";
+import burdahArab from "./nasheeds/burdah_arb.txt";
+import burdahEng from "./nasheeds/burdah_eng.txt";
+import burdahRom from "./nasheeds/burdah_rom.txt";
+import ya_hadiyArab from "./nasheeds/ya-hadiy_arb.txt";
+import ya_hadiyEng from "./nasheeds/ya-hadiy_eng.txt";
+import ya_hadiyRom from "./nasheeds/ya-hadiy_rom.txt";
+import ishrabArab from "./nasheeds/ishrab_arb.txt";
+import ishrabEng from "./nasheeds/ishrab_eng.txt";
+import ishrabRom from "./nasheeds/ishrab_rom.txt";
+import ya_hananaArab from "./nasheeds/ya-hanana_arb.txt";
+import ya_hananaEng from "./nasheeds/ya-hanana_eng.txt";
+import ya_hananaRom from "./nasheeds/ya-hanana_rom.txt";
+import talalArab from "./nasheeds/talal_arb.txt";
+import talalEng from "./nasheeds/talal_eng.txt";
+import talalRom from "./nasheeds/talal_rom.txt";
+import hamdArab from "./nasheeds/hamd_arb.txt";
+import hamdEng from "./nasheeds/hamd_eng.txt";
+import hamdRom from "./nasheeds/hamd_rom.txt";
+import lamyatiArab from "./nasheeds/lamyati_arb.txt";
+import lamyatiEng from "./nasheeds/lamyati_eng.txt";
+import lamyatiRom from "./nasheeds/lamyati_rom.txt";
+import qadkafaniArab from "./nasheeds/qadkafani_arb.txt";
+import qadkafaniEng from "./nasheeds/qadkafani_eng.txt";
+import qadkafaniRom from "./nasheeds/qadkafani_rom.txt";
+import yaalimanArab from "./nasheeds/yaaliman_arb.txt";
+import yaalimanEng from "./nasheeds/yaaliman_eng.txt";
+import yaalimanRom from "./nasheeds/yaaliman_rom.txt";
+import salliyasalaamArab from "./nasheeds/salliyasalaam_arb.txt";
+import salliyasalaamEng from "./nasheeds/salliyasalaam_eng.txt";
+import salliyasalaamRom from "./nasheeds/salliyasalaam_rom.txt";
+import araftuArab from "./nasheeds/araftu_arb.txt";
+import araftuEng from "./nasheeds/araftu_eng.txt";
+import araftuRom from "./nasheeds/araftu_rom.txt";
+import salaamArab from "./nasheeds/salaam_arb.txt";
+import salaamEng from "./nasheeds/salaam_eng.txt";
+import salaamRom from "./nasheeds/salaam_rom.txt";
+import assubhuArab from "./nasheeds/assubhu_arb.txt";
+import assubhuEng from "./nasheeds/assubhu_eng.txt";
+import assubhuRom from "./nasheeds/assubhu_rom.txt";
+import bihayathbutArab from "./nasheeds/bihayathbut_arb.txt";
+import bihayathbutEng from "./nasheeds/bihayathbut_eng.txt";
+import bihayathbutRom from "./nasheeds/bihayathbut_rom.txt";
+import madadArab from "./nasheeds/madad_arb.txt";
+import madadEng from "./nasheeds/madad_eng.txt";
+import madadRom from "./nasheeds/madad_rom.txt";
+import yanabiArab from "./nasheeds/yanabi_arb.txt";
+import yanabiEng from "./nasheeds/yanabi_eng.txt";
+import yanabiRom from "./nasheeds/yanabi_rom.txt";
+import yarabbiArab from "./nasheeds/yarabbi_arb.txt";
+import yarabbiEng from "./nasheeds/yarabbi_eng.txt";
+import yarabbiRom from "./nasheeds/yarabbi_rom.txt";
+import faslonArab from "./nasheeds/faslon_arb.txt";
+import faslonEng from "./nasheeds/faslon_eng.txt";
+import faslonRom from "./nasheeds/faslon_rom.txt";
+import iduArab from "./nasheeds/idu_arb.txt";
+import iduEng from "./nasheeds/idu_eng.txt";
+import iduRom from "./nasheeds/idu_rom.txt";
+import haqiqatArab from "./nasheeds/haqiqat_arb.txt";
+import haqiqatEng from "./nasheeds/haqiqat_eng.txt";
+import haqiqatRom from "./nasheeds/haqiqat_rom.txt";
+import ulfatArab from "./nasheeds/ulfat_arb.txt";
+import ulfatEng from "./nasheeds/ulfat_eng.txt";
+import ulfatRom from "./nasheeds/ulfat_rom.txt";
+import zaminArab from "./nasheeds/zamin_arb.txt";
+import zaminEng from "./nasheeds/zamin_eng.txt";
+import zaminRom from "./nasheeds/zamin_rom.txt";
+import saliqaArab from "./nasheeds/saliqa_arb.txt";
+import saliqaEng from "./nasheeds/saliqa_eng.txt";
+import saliqaRom from "./nasheeds/saliqa_rom.txt";
+import huzurArab from "./nasheeds/huzur_arb.txt";
+import huzurEng from "./nasheeds/huzur_eng.txt";
+import huzurRom from "./nasheeds/huzur_rom.txt";
+import paighamArab from "./nasheeds/paigham_arb.txt";
+import paighamEng from "./nasheeds/paigham_eng.txt";
+import paighamRom from "./nasheeds/paigham_rom.txt";
+import yaNabiArab from "./nasheeds/ya-nabi_arb.txt";
+import yaNabiEng from "./nasheeds/ya-nabi_eng.txt";
+import yaNabiRom from "./nasheeds/ya-nabi_rom.txt";
+import zaheArab from "./nasheeds/zahe_arb.txt";
+import zaheEng from "./nasheeds/zahe_eng.txt";
+import zaheRom from "./nasheeds/zahe_rom.txt";
+import kaabeArab from "./nasheeds/kaabe_arb.txt";
+import kaabeEng from "./nasheeds/kaabe_eng.txt";
+import kaabeRom from "./nasheeds/kaabe_rom.txt";
+import jabArab from "./nasheeds/jab_arb.txt";
+import jabEng from "./nasheeds/jab_eng.txt";
+import jabRom from "./nasheeds/jab_rom.txt";
+import rokArab from "./nasheeds/rok_arb.txt";
+import rokEng from "./nasheeds/rok_eng.txt";
+import rokRom from "./nasheeds/rok_rom.txt";
+import nakahinArab from "./nasheeds/nakahin_arb.txt";
+import nakahinEng from "./nasheeds/nakahin_eng.txt";
+import nakahinRom from "./nasheeds/nakahin_rom.txt";
+import kawakibArab from "./nasheeds/kawakib_arb.txt";
+import kawakibEng from "./nasheeds/kawakib_eng.txt";
+import kawakibRom from "./nasheeds/kawakib_rom.txt";
+import qamarunArab from "./nasheeds/qamarun_arb.txt";
+import qamarunEng from "./nasheeds/qamarun_eng.txt";
+import qamarunRom from "./nasheeds/qamarun_rom.txt";
+import imamArab from "./nasheeds/imam_arb.txt";
+import imamEng from "./nasheeds/imam_eng.txt";
+import imamRom from "./nasheeds/imam_rom.txt";
+import bashhirArab from "./nasheeds/bashhir_arb.txt";
+import bashhirEng from "./nasheeds/bashhir_eng.txt";
+import bashhirRom from "./nasheeds/bashhir_rom.txt";
+import khudiArab from "./nasheeds/khudi_arb.txt";
+import khudiEng from "./nasheeds/khudi_eng.txt";
+import khudiRom from "./nasheeds/khudi_rom.txt";
+import jashneArab from "./nasheeds/jashn_arb.txt";
+import jashneEng from "./nasheeds/jashn_eng.txt";
+import jashneRom from "./nasheeds/jashn_rom.txt";
 
 let urls = [
-araftuArab, araftuEng, araftuRom, assubhuArab, assubhuEng, assubhuRom, bashhirArab,bashhirEng, bashhirRom, bihayathbutArab, bihayathbutEng, bihayathbutRom,
-faslonArab,faslonEng,faslonRom, haqiqatArab,haqiqatEng,haqiqatRom,huzurArab, huzurEng, huzurRom, iduArab, iduEng, iduRom, 
-ishrabArab, ishrabEng, ishrabRom, jabArab, jabEng,jabRom, kaabeArab, kaabeEng, kaabeRom, saliqaArab, saliqaEng, saliqaRom,
-lamyatiArab, lamyatiEng, lamyatiRom, hamdArab, hamdEng, hamdRom, nakahinArab, nakahinEng, nakahinRom, madadArab, madadEng, madadRom,
-ulfatArab, ulfatEng, ulfatRom, qadkafaniArab, qadkafaniEng, qadkafaniRom, qamarunArab,qamarunEng,qamarunRom, burdahArab, burdahEng, burdahRom,
-muhammadiyaArab, muhammadiyaEng, muhammadiyaRom, paighamArab, paighamEng, paighamRom, rokArab, rokEng, rokRom, 
-salaamArab, salaamEng, salaamRom, badrArab, badrEng, badrRom, kawakibArab, kawakibEng, kawakibRom, salliyasalaamArab, salliyasalaamEng, salliyasalaamRom, 
-talalArab, talalEng, talalRom, talamaArab, talamaEng, talamaRom, yaalimanArab, yaalimanEng, yaalimanRom, 
-ya_hadiyArab, ya_hadiyEng, ya_hadiyRom, ya_hananaArab, ya_hananaEng, ya_hananaRom, imamArab, imamEng,imamRom, yaNabiArab, yaNabiEng, yaNabiRom, 
-yanabiArab, yanabiEng, yanabiRom, yarabbiArab, yarabbiEng, yarabbiRom, zaheArab, zaheEng, zaheRom, zaminArab, zaminEng, zaminRom, 
-
-]
+  araftuArab,
+  araftuEng,
+  araftuRom,
+  assubhuArab,
+  assubhuEng,
+  assubhuRom,
+  bashhirArab,
+  bashhirEng,
+  bashhirRom,
+  bihayathbutArab,
+  bihayathbutEng,
+  bihayathbutRom,
+  faslonArab,
+  faslonEng,
+  faslonRom,
+  jashneArab,
+  jashneEng,
+  jashneRom,
+  haqiqatArab,
+  haqiqatEng,
+  haqiqatRom,
+  huzurArab,
+  huzurEng,
+  huzurRom,
+  iduArab,
+  iduEng,
+  iduRom,
+  ishrabArab,
+  ishrabEng,
+  ishrabRom,
+  jabArab,
+  jabEng,
+  jabRom,
+  kaabeArab,
+  kaabeEng,
+  kaabeRom,
+  khudiArab,
+  khudiEng,
+  khudiRom,
+  saliqaArab,
+  saliqaEng,
+  saliqaRom,
+  lamyatiArab,
+  lamyatiEng,
+  lamyatiRom,
+  hamdArab,
+  hamdEng,
+  hamdRom,
+  nakahinArab,
+  nakahinEng,
+  nakahinRom,
+  madadArab,
+  madadEng,
+  madadRom,
+  ulfatArab,
+  ulfatEng,
+  ulfatRom,
+  qadkafaniArab,
+  qadkafaniEng,
+  qadkafaniRom,
+  qamarunArab,
+  qamarunEng,
+  qamarunRom,
+  burdahArab,
+  burdahEng,
+  burdahRom,
+  muhammadiyaArab,
+  muhammadiyaEng,
+  muhammadiyaRom,
+  paighamArab,
+  paighamEng,
+  paighamRom,
+  rokArab,
+  rokEng,
+  rokRom,
+  salaamArab,
+  salaamEng,
+  salaamRom,
+  badrArab,
+  badrEng,
+  badrRom,
+  kawakibArab,
+  kawakibEng,
+  kawakibRom,
+  salliyasalaamArab,
+  salliyasalaamEng,
+  salliyasalaamRom,
+  talalArab,
+  talalEng,
+  talalRom,
+  talamaArab,
+  talamaEng,
+  talamaRom,
+  yaalimanArab,
+  yaalimanEng,
+  yaalimanRom,
+  ya_hadiyArab,
+  ya_hadiyEng,
+  ya_hadiyRom,
+  ya_hananaArab,
+  ya_hananaEng,
+  ya_hananaRom,
+  imamArab,
+  imamEng,
+  imamRom,
+  yaNabiArab,
+  yaNabiEng,
+  yaNabiRom,
+  yanabiArab,
+  yanabiEng,
+  yanabiRom,
+  yarabbiArab,
+  yarabbiEng,
+  yarabbiRom,
+  zaheArab,
+  zaheEng,
+  zaheRom,
+  zaminArab,
+  zaminEng,
+  zaminRom,
+];
 let newUrls = [];
-for (let i = 0; i < urls.length; i+=3){
-	newUrls.push([urls[i],urls[i+1],urls[i+2]])
+for (let i = 0; i < urls.length; i += 3) {
+  newUrls.push([urls[i], urls[i + 1], urls[i + 2]]);
 }
 
- export const generateNasheed = async () => {
-	let nasheed = [];
-		await Promise.all(newUrls.map(async (nash, index) =>{
-				await fetch(nash[0]).then((response) => response.text())
-					.then((result) => {
-						const linesArr = result.split("\n")
-						nasheed[index] = {arabTitle: linesArr[0]}
-						nasheed[index].arab = linesArr.slice(1)});
-				await fetch(nash[1]).then((response) => response.text())
-					.then((result) => {
-						const linesArr = result.split("\n")
-						nasheed[index].eng = linesArr});
-				await fetch(nash[2]).then((response) => response.text())
-					.then((result) => {
-						const linesArr = result.split("\n")
-						nasheed[index].engTitle = linesArr[0]
-						nasheed[index].rom = linesArr.slice(1)});
-				}
-			)
-		)
-	return {nasheed}
-}
+export const generateNasheed = async () => {
+  let nasheed = [];
+  await Promise.all(
+    newUrls.map(async (nash, index) => {
+      await fetch(nash[0])
+        .then((response) => response.text())
+        .then((result) => {
+          const linesArr = result.split("\n");
+          nasheed[index] = { arabTitle: linesArr[0] };
+          nasheed[index].arab = linesArr.slice(1);
+        });
+      await fetch(nash[1])
+        .then((response) => response.text())
+        .then((result) => {
+          const linesArr = result.split("\n");
+          nasheed[index].eng = linesArr;
+        });
+      await fetch(nash[2])
+        .then((response) => response.text())
+        .then((result) => {
+          const linesArr = result.split("\n");
+          nasheed[index].engTitle = linesArr[0];
+          nasheed[index].rom = linesArr.slice(1);
+        });
+    })
+  );
+  return { nasheed };
+};
